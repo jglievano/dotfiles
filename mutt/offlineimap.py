56a2a924480983948da08437e21178826c0d11ad
Gabriel Lievano <gabriell@google.com>
2015-12-15 16:12:31 -0800

    Do not print gpg path.
    offlineimap.py was doing it. It doesn't have to.

--- mutt/offlineimap.py
+++ mutt/offlineimap.py
@@ -7,7 +7,6 @@ from os.path import expanduser
 def mailpasswd(acct):
     acct = os.path.basename(acct)
     path = "%s/.%s.gpg" % (expanduser("~"), acct)
-    print "%s" % path
     args = ["gpg", "--quiet", "--for-your-eyes-only", "--no-tty", "-d", path]
     try:
         return subprocess.check_output(args).strip()
