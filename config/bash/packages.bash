4d485ed91ccfe878b62d39945f50a9ebf0e5d9d0
Gabriel Lievano <jglievano@wolfmango.com>
2019-12-09 13:15:45 -0800

    Add bash_completion setup.

--- config/bash/packages.bash
+++ config/bash/packages.bash
@@ -1,2 +1,6 @@
 check_cmd pyenv && eval "$(pyenv init -)"
 check_cmd rbenv && eval "$(rbenv init -)"
+
+if [ -f $(brew --prefix)/etc/bash_completion ]; then
+	. $(brew --prefix)/etc/bash_completion
+fi
